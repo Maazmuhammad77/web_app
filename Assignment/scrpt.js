e4d9ee6270b4ae5a5567051ec70188e994540ac5
maazmuhammad77 <maazmuhammad77@gmail.com>
2024-09-04 12:05:18 +0500

    Adding Code

---
+++ Assignment/scrpt.js
@@ -0,0 +1,49 @@
+// let stdent = "uzair"
+// console.log(stdent)
+// let a = 3
+// let b = 9
+// console.log((a + b) * 10)
+// let fruits = ["Apple", "Mango", "Banana"]
+// fruits.shift()
+// console.log(fruits)
+
+// let array = ["one", "two", "three", "four", "five"]
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i])
+// }
+// let firstName = ["Uzair ", "Maaz ", "Haroon "]
+// let lastName = ["Sharsham", "muhammad", "Rasheed"]
+// let fullName = []
+// for (let i = 0; i < firstName.length; i++) {
+
+//     for (let j = 0; j < lastName.length; j++) {
+//         if (i == j) {
+//             fullName.push(firstName[i] + lastName[j])
+//         }
+//     }
+
+// }
+// console.log(fullName)
+// let firstN = "Maaz"
+// console.log(firstN.length)
+// console.log(firstN.toUpperCase())
+// console.log(firstN.toLowerCase())
+
+// let numbers = ["one", "two", "three", "Four"]
+// numbers.splice(0, 2, "3", "4", "5")
+// console.log(numbers)
+
+// let num = ["1", "2", "3", "4"]
+// let nonum = num.slice(1, 3)
+// console.log(nonum)
+
+// let myname = "maazMuhammad"
+
+// for (let i = 0; i <= 11; i++) {
+//     if (i == 0) {
+//         console.log(myname[i].toUpperCase())
+//         console.log(myname[i])
+//     } else { console.log(myname[i]) }
+// }
+let xyz = "maazmuhammad"
+console.log(xyz[0].toUpperCase() + xyz.slice(5))
